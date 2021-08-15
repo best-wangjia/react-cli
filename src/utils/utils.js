ff3ee9cff11fccdfde6b91a37ed5240e1a9037af
Wang Jia <wangjia125513819@foxmail.com>
2021-08-15 13:36:18 +0800

    chore:增加babel属性

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -430,7 +430,21 @@ let cookies = {
     return aKeys
   }
 }
+// base64中文转码
+let Base64 = {
+  encode(str) {
+      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+          function toSolidBytes(match, p1) {
+              return String.fromCharCode('0x' + p1)
+          }))
+  },
+  decode(str) {
+      return decodeURIComponent(atob(str).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      }).join(''))
+  }
+}
 
 export {
-  fillZero, getRandom, cookies
+  fillZero, getRandom, cookies, Base64
 }
