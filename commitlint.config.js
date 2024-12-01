9160c9ec6603d2ee6e08a3405ea29ff528a4fff2
wangjia <wangjia125513819@foxmail.com>
2024-12-01 20:48:08 +0800

    chore: 修改配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,19 +8,20 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // 新功能 feature
-        'fix', // 修复 bug
-        'docs', // 文档注释
-        'style', // 代码格式(不影响代码运行的变动)
-        'refactor', // 重构(既不增加新功能，也不是修复bug)
-        'perf', // 性能优化
-        'test', // 增加测试
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // 回退
-        'build', // 打包
-        'modify', // 修改
-      ],
+        'feat', // 增加新功能
+        'fix', // 修复问题/BUG
+        'style', // 代码风格相关无影响运行结果的
+        'perf', // 优化/性能提升
+        'refactor', // 重构(既不是增加feature，也不是修复bug)
+        'revert', // 撤销修改
+        'test', // 测试相关
+        'docs', // 文档/注释
+        'chore', // 依赖更新/脚手架配置修改等
+        'ci', // 持续集成
+        'types', // 类型定义文件更改
+        'wip' // 开发中
+      ]
     ], // subject 大小写不做校验
-    'subject-case': [0],
-  },
+    'subject-case': [0]
+  }
 }
